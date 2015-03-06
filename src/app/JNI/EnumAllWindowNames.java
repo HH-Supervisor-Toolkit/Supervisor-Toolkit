7a776ceb50a6421493adcf825a21f761ae30186e
haywoosd <haywoosd@HAYWOOSD-1.rose-hulman.edu>
2015-03-06 15:40:58 -0500

    Changed cLib.dll so that it is smaller.

--- src/app/JNI/EnumAllWindowNames.java
+++ src/app/JNI/EnumAllWindowNames.java
@@ -13,7 +13,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
