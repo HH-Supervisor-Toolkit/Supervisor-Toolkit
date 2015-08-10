58f374c9756fcb9059a984b762fc5f0229111e63
haywoodsloan <haywood.sloan@gmail.com>
2015-08-09 22:48:06 -0400

    Replaced use of Thread.sleep() with TimerTasks in regards to the refresh timer.

--- src/app/timer/BrowserTimerThread.java
+++ src/app/timer/BrowserTimerThread.java
@@ -2,8 +2,8 @@ package app.timer;
 
 import app.browser.ExtendedWebBrowser;
 import app.main;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Timer;
+import java.util.TimerTask;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -11,9 +11,13 @@ import javafx.beans.value.ObservableValue;
 public class BrowserTimerThread extends Thread {
 
     int timerDuration;
+    long lastRefreshTime = System.currentTimeMillis();
+
     ExtendedWebBrowser webBrowser;
-    boolean terminated = false;
-    long lastRefreshTime;
+    TimerWarningDialog timerDialog = null;
+    Timer alertTimer = new Timer();
+    TimerAlertTask timerAlertTask = new TimerAlertTask();
+    ChangeListener<Number> refreshListener = null;
 
     public BrowserTimerThread(int minutes, ExtendedWebBrowser webBrowser2) {
         timerDuration = minutes;
@@ -21,69 +25,70 @@ public class BrowserTimerThread extends Thread {
     }
 
     public void terminate() {
-        terminated = true;
+
+        alertTimer.cancel();
+
+        Platform.runLater(() -> {
+            webBrowser.getEngine().getLoadWorker().workDoneProperty().removeListener(refreshListener);
+        });
     }
 
     @Override
     public void run() {
 
-        ChangeListener<Number> refreshListener = (ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
+        refreshListener = (ObservableValue<? extends Number> observable, Number oldValue, Number newValue) -> {
 
             if (newValue.intValue() == 100) {
                 System.out.println("A timer has seen a change of page");
                 lastRefreshTime = System.currentTimeMillis();
+
+                timerAlertTask.cancel();
+                timerAlertTask = new TimerAlertTask();
+                alertTimer.scheduleAtFixedRate(timerAlertTask, timerDuration * 40000, timerDuration * 10000);
             }
 
         };
 
+        alertTimer.scheduleAtFixedRate(timerAlertTask, timerDuration * 40000, timerDuration * 10000);
+
         Platform.runLater(() -> {
             webBrowser.getEngine().getLoadWorker().workDoneProperty().addListener(refreshListener);
         });
-
-        long timeDifference;
-        TimerWarningDialog timerDialog = null;
-        lastRefreshTime = System.currentTimeMillis();
-
-        while (!terminated) {
-
-            timeDifference = System.currentTimeMillis() - lastRefreshTime;
-
-            if (timeDifference > timerDuration * 35000 && timeDifference < timerDuration * 65000) {
-
-                if (timerDialog != null && timerDialog.isVisible()) {
-                    
-                    timerDialog.updateMessage("Your timer for " + webBrowser.getName() + " is at " + limitDoublePercision((double) timeDifference / (60000), 2) + " of " + timerDuration + " minutes");
-                    timerDialog.pack();
-
-                } else {
-
-                    timerDialog = new TimerWarningDialog(main.frame, false, "Your timer for " + webBrowser.getName() + " is at " + limitDoublePercision((double) timeDifference / (60000), 2) + " of " + timerDuration + " minutes", webBrowser);
-                    timerDialog.setLocationRelativeTo(main.frame);
-                    timerDialog.setVisible(true);
-                    timerDialog.setAlwaysOnTop(true);
-                    timerDialog.setAlwaysOnTop(false);
-                }
-                
-                System.out.println("A notice about the timer for " + webBrowser.getName() + " has been given.");
-            }
-
-            try {
-                Thread.sleep(timerDuration * 10000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(BrowserTimerThread.class.getName()).log(Level.SEVERE, null, ex);
-            }
-
-        }
-
-        Platform.runLater(() -> {
-            webBrowser.getEngine().getLoadWorker().workDoneProperty().removeListener(refreshListener);
-        });
-
     }
 
     public double limitDoublePercision(double d, int decimals) {
         int d1 = (int) (d * Math.pow(10, decimals));
-        double d2 = d1 / Math.pow(10, decimals);
-        return d2;
+        return (double) d1 / Math.pow(10, decimals);
     }
+
+    private class TimerAlertTask extends TimerTask {
+
+        @Override
+        public void run() {
+
+            long timeDifference = System.currentTimeMillis() - lastRefreshTime;
+
+            if (timerDialog != null && timerDialog.isVisible()) {
+
+                timerDialog.updateMessage("Your timer for " + webBrowser.getName() + " is at " + limitDoublePercision((double) timeDifference / (60000), 2) + " of " + timerDuration + " minutes");
+                timerDialog.pack();
+
+            } else {
+
+                timerDialog = new TimerWarningDialog(main.frame, false, "Your timer for " + webBrowser.getName() + " is at " + limitDoublePercision((double) timeDifference / (60000), 2) + " of " + timerDuration + " minutes", webBrowser);
+
+                timerDialog.setLocationRelativeTo(main.frame);
+                timerDialog.setVisible(true);
+                timerDialog.setAlwaysOnTop(true);
+                timerDialog.setAlwaysOnTop(false);
+            }
+
+            System.out.println("A notice about the timer for " + webBrowser.getName() + " has been given.");
+
+            if (timeDifference >= timerDuration * 60000) {
+                this.cancel();
+            }
+        }
+    }
+
 }
